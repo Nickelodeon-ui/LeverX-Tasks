928c538530aa295ad6709dceced744e740970718
Nick <borodachnikolay@mail.ru>
2021-05-23 21:07:39 +0300

    SRP, format validation, reduced complexity

--- Task_1/Task_1.py
+++ Task_1/Task_1.py
@@ -7,53 +7,62 @@ class PathsEqualError(Exception):
     def __str__(self):
         return "Two files have the same path"
 
-class BaseStudentsRoomsManager():
+class FileManager():
     def __init__(self, stud_file_path, rooms_file_path):
         self.stud_file_path = stud_file_path
         self.rooms_file_path = rooms_file_path
 
-    def combine_data(self):
+    def open_rooms_file(self):
+        try:
+            with open(Path(self.rooms_file_path), "r") as rooms_file:
+                rooms_data = json.load(rooms_file)
+            return rooms_data
+        except FileNotFoundError:
+            self.rooms_file_path = input("Enter the correct path to rooms file: ")
+            if self.rooms_file_path and self.rooms_file_path[0] in ('\'', "\""):
+                self.rooms_file_path = self.rooms_file_path[1:-1]
+            if self.stud_file_path == self.rooms_file_path:
+                raise PathsEqualError
+            return self.open_rooms_file()
+
+    def open_students_file(self):
         try:
             with open(Path(self.stud_file_path), "r") as stud_file:
                 stud_data = json.load(stud_file)
+                return stud_data
         except FileNotFoundError:
             self.stud_file_path = input("Enter the correct path to students file: ")
             # Check if user entered path with or without starting ' or " to work correctly
             # because input puts these at the beginning by default
-            if self.stud_file_path[0] in ('\'', "\""):
+            if self.stud_file_path and self.stud_file_path[0] in ('\'', "\""):
                 self.stud_file_path = self.stud_file_path[1:-1]
             # Check if two paths equal each other
             if self.stud_file_path == self.rooms_file_path:
                 raise PathsEqualError
-            return self.combine_data()
-        
-        try:
-            with open(Path(self.rooms_file_path), "r") as rooms_file:
-                rooms_data = json.load(rooms_file)
-        except FileNotFoundError:
-            self.rooms_file_path = input("Enter the correct path to rooms file: ")
-            if self.rooms_file_path[0] in ('\'', "\""):
-                self.rooms_file_path = self.rooms_file_path[1:-1]
-            if self.stud_file_path == self.rooms_file_path:
-                raise PathsEqualError
-            return self.combine_data()
-        
-        combined_data = {}
-        
-        try:
-            for room in rooms_data:
-                for stud in stud_data:
-                    if stud["room"] == room["id"]:
-                        if room["id"] not in combined_data.keys():
-                            combined_data[room["id"]] = [stud["name"],]
-                        else:
-                            combined_data[room["id"]].append(stud["name"])
+            return self.open_students_file()    
 
-            self.combined_data = combined_data
-        except Exception as e:
-            return e.__traceback__
-        return "The data was processed successfully!"
-    
+class CombineDataManager():
+    def __init__(self, stud_data, room_data):
+        self.stud_data = stud_data
+        self.rooms_data = room_data
+
+    def combine_data(self):
+            for student in self.stud_data:
+                room = student["room"]
+                name = student["name"]
+                room_data = self.rooms_data[room]
+                if "students" not in room_data.keys():
+                    room_data["students"] = [name,]
+                else:
+                    room_data["students"].append(name)
+            return self.rooms_data
+
+class BaseStudentsRoomsManager():
+    def __init__(self, stud_file_path, rooms_file_path):
+        file_manager = FileManager(stud_file_path, rooms_file_path)
+        combine_manager = CombineDataManager(file_manager.open_students_file(), file_manager.open_rooms_file())
+        self.combined_data = combine_manager.combine_data()
+          
     def output_data(self):
         raise NotImplementedError ("Subclass must contain output_data method")
 
@@ -66,25 +75,31 @@ class JSONStudentsRoomsManager(BaseStudentsRoomsManager):
 class XMLStudentsRoomsManager(BaseStudentsRoomsManager):
     def output_data(self):
         root = xml.Element("rooms")
-        for room_stud_pair in self.combined_data.items():
-            room_id = str(room_stud_pair[0])
-            student_names = room_stud_pair[1]
+        for room in self.combined_data:
+            room_id = str(room["id"])
+            student_names = room["students"]
             room = xml.Element("room", attrib={"id": room_id})
             root.append(room)
             for student_name in student_names:
                 student = xml.SubElement(room, "student")
                 student.text = student_name
-        
+                
         tree = xml.ElementTree(root)
         with open("output.xml", "wb") as output_file:
             tree.write(output_file)
         return "The report was formed!"
 
+def validate_input_format(ftype):
+    availiable_formats = ["json", "xml"]
+    if ftype not in availiable_formats:
+        raise argparse.ArgumentTypeError(f"{ftype} is an invalid output format")
+    return ftype
+
 def get_parser():
     parser = argparse.ArgumentParser()
     parser.add_argument("-students", required=True)
     parser.add_argument("-rooms", required=True)
-    parser.add_argument("-format", default="json")
+    parser.add_argument("-format", type=validate_input_format, default="json")
     return parser
 
 if __name__ == "__main__":    
@@ -101,16 +116,13 @@ if __name__ == "__main__":
 
     format_manager_dict = {
         "json": JSONStudentsRoomsManager(stud_file_path, rooms_file_path),
-        "xml": XMLStudentsRoomsManager(stud_file_path, rooms_file_path)
+        "xml": XMLStudentsRoomsManager(stud_file_path, rooms_file_path),
         }
     
     manager = format_manager_dict.get(output_format, False)
     
     if not manager:
         raise NotImplementedError("We don't support this output format yet")
-    
-    output = manager.combine_data()
-    print(output)
 
     output = manager.output_data()
     print(output)
